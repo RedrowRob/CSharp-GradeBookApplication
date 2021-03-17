f6067c4a34ca2fe01be8e3d2640d16f331af6ee6
RedrowRob <rob.armstrong@redrow.co.uk>
2021-03-17 10:29:26 +0000

    Module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -8,6 +10,39 @@ namespace GradeBook.GradeBooks
             : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            var studentsPer20Percent = (int)Math.Round(Students.Count / 5.0);
+            var orderedGrades = Students.OrderByDescending(x => x.AverageGrade).Select(x => x.AverageGrade).ToList();
+            char result;
+
+            switch (averageGrade)
+            {
+                case var grade when grade >= orderedGrades[studentsPer20Percent - 1]:
+                    result = 'A';
+                    break;
+                case var grade when grade >= orderedGrades[2 * studentsPer20Percent - 1]:
+                    result = 'B';
+                    break;
+                case var grade when grade >= orderedGrades[3 * studentsPer20Percent - 1]:
+                    result = 'C';
+                    break;
+                case var grade when grade >= orderedGrades[4 * studentsPer20Percent - 1]:
+                    result = 'D';
+                    break;
+                default:
+                    result = 'F';
+                    break;
+            }
+
+            return result;
         }
     }
 }
